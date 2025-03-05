5524f69324b6fd7be393d521f24e60865669d7c3
brownhounds <brownhounds739@gmail.com>
2025-03-05 20:47:20 +0000

    Remove wait on presenter

--- src/Present.ts
+++ src/Present.ts
@@ -23,14 +23,6 @@ export class Present {
         return this.rootElement as Component;
     }
 
-    public wait(timeout = 0): Promise<void> {
-        return new Promise((resolve) => {
-            setTimeout(() => {
-                resolve();
-            }, timeout);
-        });
-    }
-
     public getByTag<Component extends HTMLElement>(tag: string): Component {
         return query(this.get(), tag) as Component;
     }
